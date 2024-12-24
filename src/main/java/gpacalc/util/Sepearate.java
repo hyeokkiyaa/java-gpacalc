b7961ab6d36ece728960398a32ae0c6bdc0cba05
hyeokkiyaa <khm38607574@gmail.com>
2024-12-24 16:55:33 +0900

    exception handling made but not perfect

--- src/main/java/gpacalc/util/Sepearate.java
+++ src/main/java/gpacalc/util/Sepearate.java
@@ -21,8 +21,22 @@ public class Sepearate {
         for (String s: subjects) {
             st = new StringTokenizer(s, "-");
             String subjectName = st.nextToken();
-            int credit = Integer.parseInt(st.nextToken());
-            String score = st.nextToken();
+            int credit = 0;
+            String score = "";
+            try {
+                credit = Integer.parseInt(st.nextToken());
+                if (credit < 1 || credit > 4) {
+                    throw new IllegalArgumentException("학점은 1부터 4까지만");
+                }
+                score = st.nextToken();
+                if (!score.equals("A+") && !score.equals("A0") && !score.equals("B+") && !score.equals("B0") && !score.equals("C+") && !score.equals("C0") && !score.equals("D+") && !score.equals("D0") && !score.equals("F") && !score.equals("P") && !score.equals("NP")) {
+                    throw new IllegalArgumentException("평점이 이상한디,,,");
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println("예외가 발생: "  + e.getMessage());
+                System.exit(0);
+            }
+
             result.add(new Subject(subjectName, credit, score));
         }
         return result;
